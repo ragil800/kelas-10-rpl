c666b63b40909c2e3a7d0c1332576017f57b734e
ragil800 <ragilcreator800@gmail.com>
2025-10-16 10:10:19 +0700

    Add files via upload

---
+++ X RPL/Kelas/16 okt 2025/pages/kontak.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+    <input type="text" name="nama" placeholder="nama lengkap">
+    <input type="text" name="pesan" placeholder="pesan">
+    <input type="email" name="email" placeholder="email">
+</form>
