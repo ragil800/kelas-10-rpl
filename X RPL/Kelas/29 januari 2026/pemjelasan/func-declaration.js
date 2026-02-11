dd8dc88209b4a05804cde439025fcca8da17c100
ragil800 <ragilcreator800@gmail.com>
2026-02-11 22:46:44 +0700

    Add files via upload

---
+++ X RPL/Kelas/29 januari 2026/pemjelasan/func-declaration.js
@@ -0,0 +1,5 @@
+function sapa(){ console.log('halo'); }
+        function tambah(a, b){ return a+b }
+        function kurang(a, b){ return a-b }
+        function kali(a,b) {return a*b}
+        function bagi(a,b) {return a/b}
